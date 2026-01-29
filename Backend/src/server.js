d75c0e0e938f1d52de9a98c6615d958bbbcd397d
Dishant Patil <patildishant312@gmail.com>
2026-01-29 11:05:49 +0530

    deploying the service

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -11,14 +11,14 @@ const app = express();
 const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 3000;
-    
+
 app.use('/api/auth', authRoutes);
 app.use('/api/message', messageRoutes);
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+    app.use(express.static(path.join(__dirname, '../Frontend/dist')));
     app.get('*', (_,res) => {
-        res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+        res.sendFile(path.join(__dirname, '../Frontend/dist/index.html'));
     });
 }     
 
