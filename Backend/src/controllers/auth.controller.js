1820c5979803163eb62a07f84cd4953012f2bd8c
Dishant Patil <patildishant312@gmail.com>
2026-02-04 14:43:15 +0530

    Welcome email sent successfully

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,6 +1,8 @@
+import { sendWelcomeEmail } from '../emails/emailHandlers.js';
 import { generateToken } from '../lib/utils.js';
 import User from '../models/User.js'; 
 import bcrypt from 'bcryptjs';
+import { ENV } from '../lib/env.js';        
 
 export const signup = async (req,res) => {
     const {fullName, email, password} = req.body;
@@ -37,6 +39,13 @@ export const signup = async (req,res) => {
                 email: newUser.fullName,
                 profilePic: newUser.profilePic,
             });
+
+            try{
+                await sendWelcomeEmail(savedUser.email, savedUser.fullName, ENV.CLIENT_URL);
+            } catch(error){
+                console.error("Failed to send welcome email:", error);
+            }
+
         }else{
             res.status(400).json({message: "Invalid User Data"});
         }
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,25 +1,23 @@
 import express from 'express';
-import dotenv from 'dotenv';
 import path from 'path';
+import { ENV } from './lib/env.js';     
 
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 import { connect } from 'http2';
 import { connectDB } from './lib/db.js';
 
-dotenv.config();
-
 const app = express();
 const __dirname = path.resolve();
 
-const PORT = process.env.PORT || 3000;
+const PORT = ENV.PORT || 3000;
 
 app.use(express.json());    
 
 app.use('/api/auth', authRoutes);
 app.use('/api/message', messageRoutes);
 
-if(process.env.NODE_ENV === 'production'){
+if(ENV.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname, '../Frontend/dist')));
     app.get('*', (_,res) => {
         res.sendFile(path.join(__dirname, '../Frontend/dist/index.html'));
