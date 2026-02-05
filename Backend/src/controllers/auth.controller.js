663fd99247d1173635a117454a4d7a18606bb85c
Dishant Patil <patildishant312@gmail.com>
2026-02-05 12:46:18 +0530

    Login and Logout Functionality Implemented

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -54,3 +54,38 @@ export const signup = async (req,res) => {
         res.status(500).json({message: "Internal server error"});
     }
 }
+
+export const login = async (req,res) => {
+    const {email,password} = req.body;
+
+    if(!email||!password){
+        res.status(400).json({message: "email and password are required"});
+    }
+
+    try{
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({message:"Invalid login credentials"});
+        }
+        const isPasswordCorrect = await bcrypt.compare(password,user.password);
+        if(!isPasswordCorrect){
+            return res.status(400).json({message:"Invalid login credentials"});
+        }
+        generateToken(user.id,res)
+
+        res.status(200).json({
+            _id: user._id,
+            fullname: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic,
+        });
+    }catch(error){
+        console.error("Error in login controller:",error);
+        res.status(500).json({message: "Internal server error"});
+    }
+}
+
+export const logout =(_,res) => {
+    res.cookie("jwt","",{maxAge: 0});
+    res.status(200).json({message: "Logged out Successfully"});
+}
--- Backend/src/routes/auth.route.js
+++ Backend/src/routes/auth.route.js
@@ -1,14 +1,11 @@
 import express from 'express';
-import { signup } from '../controllers/auth.controller.js';
+import { signup, login, logout } from '../controllers/auth.controller.js';
 const router = express.Router();
 
-router.get('/signin', (req,res) => {
-    res.send('SignIn Route');
-})
+router.post('/login', login);
+
 router.post('/signup', signup);
 
-router.get('/logout', (req,res) => {
-    res.send('Logout Route');
-})
+router.post('/logout', logout);
 
 export default router;
