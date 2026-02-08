a24769383cf1c1bd43cf3e41364ee8c2c44f0e79
Dishant Patil <patildishant312@gmail.com>
2026-02-08 10:20:49 +0530

    message routes added

--- Backend/src/routes/message.route.js
+++ Backend/src/routes/message.route.js
@@ -1,8 +1,16 @@
-import express from 'express';
+import express, { application } from 'express';
+import { getAllContacts, getMessageByUserId, sendMessage, getChatPartners } from '../controllers/message.controller.js';
+import { protectRoute } from '../middleware/auth.middleware.js';
+import { arcjetProtection } from '../middleware/arcjet.middleware.js';
+
 const router = express.Router();
 
-router.get('/send', (req, res) => {
-    res.send('Message send route');
-});
+router.use(arcjetProtection, protectRoute)
+
+router.get('/contacts', getAllContacts);
+router.get('/chats', getChatPartners);
+router.get('/:id', getMessageByUserId);
+router.post('/send/:id', sendMessage);
+
 
 export default router;
