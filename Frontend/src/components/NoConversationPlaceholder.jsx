379385348848e20f7534dc5b825b8b526c4751db
Dishant Patil <patildishant312@gmail.com>
2026-02-17 10:50:45 +0530

    ChatPage Setup Done

---
+++ Frontend/src/components/NoConversationPlaceholder.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NoConversationPlaceholder() {
+  return (
+    <div>
+      NoConverstaionPlaceholder
+    </div>
+  )
+}
+
+export default NoConversationPlaceholder
